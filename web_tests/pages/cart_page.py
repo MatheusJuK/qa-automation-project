1e9408d8c2009b44b37f4794cfb6172d2ba14d87
Matheus Rodrigues Jucá <matheusrjuca@gmail.com>
2026-05-01 00:46:11 -0300

    fix: implicit waiting in checkout

--- web_tests/pages/cart_page.py
+++ web_tests/pages/cart_page.py
@@ -10,11 +10,11 @@ class CartPage(BasePage):
 
     def checkout(self):
         try:
-            self.wait.until(EC.element_to_be_clickable(self.CHECKOUT_BUTTON))
+            self.driver.wait.until(EC.element_to_be_clickable(self.CHECKOUT_BUTTON))
             self.click(*self.CHECKOUT_BUTTON)
-            self.wait.until(EC.url_contains("checkout-step-one"))
+            self.driver.wait.until(EC.url_contains("checkout-step-one"))
         except:
             self.driver.get("https://www.saucedemo.com/checkout-step-one.html")
-            self.wait.until(EC.url_contains("checkout-step-one"))
-
-    
+            self.driver.implicitly_wait(10)
+    ##WebDriver nao tem wait entao o correto seria:
+    # self.driver.implicitly_wait(10)
