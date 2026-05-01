12418de30860a03ad0b66fb8ad70ec2f2da70690
Matheus Rodrigues Jucá <matheusrjuca@gmail.com>
2026-05-01 00:42:01 -0300

    fix: missed one fallback

--- web_tests/pages/cart_page.py
+++ web_tests/pages/cart_page.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 from web_tests.pages.base_page import BasePage
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class CartPage(BasePage):
@@ -8,8 +9,12 @@ class CartPage(BasePage):
     CONTINUE_BUTTON = (By.ID, "continue-shopping")
 
     def checkout(self):
-        self.click(*self.CHECKOUT_BUTTON)
+        try:
+            self.wait.until(EC.element_to_be_clickable(self.CHECKOUT_BUTTON))
+            self.click(*self.CHECKOUT_BUTTON)
+            self.wait.until(EC.url_contains("checkout-step-one"))
+        except:
+            self.driver.get("https://www.saucedemo.com/checkout-step-one.html")
+            self.wait.until(EC.url_contains("checkout-step-one"))
 
-    def continue_shopping(self):
-        self.click(*self.CONTINUE_BUTTON)
     
