63658ddda5af330e882f46e8566ac82852a39a6f
Matheus Jucá <matheusrjuca@gmail.com>
2026-05-07 18:49:06 -0300

    feat: add pop-up blocking to prevent it breaking web tests

--- web_tests/driver/driver_factory.py
+++ web_tests/driver/driver_factory.py
@@ -6,11 +6,24 @@ from selenium.webdriver.chrome.options import Options
 def create_driver():
     options = Options()
     
-    options.add_argument("--headless=new")
+#    options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
     options.add_argument("window-size=1920,1080")
+    options.add_argument("--disable-popup-blocking")
+    options.add_argument("--disable-notifications")
+    
+    prefs = {
+        "profile.default_content_setting_values.notifications": 2,
+        "credentials_enable_service": False,
+        "profile.password_manager_enabled": False,
+        "autofill.profile_enabled": False,
+        "autofill.credit_card_enabled": False,
+        "profile.password_manager_leak_detection": False
+    }
+    options.add_experimental_option("prefs", prefs)
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
         
     driver = webdriver.Chrome(
         service=Service(ChromeDriverManager().install()),
--- web_tests/tests/test_base.py
+++ web_tests/tests/test_base.py
@@ -7,3 +7,19 @@ def test_open_page():
         assert "Swag Labs" in driver.title
     finally:
         driver.quit()
+
+def test_login():
+    driver = create_driver()
+    try:
+        driver.get("https://www.saucedemo.com/")
+        username_input = driver.find_element("id", "user-name")
+        password_input = driver.find_element("id", "password")
+        login_button = driver.find_element("id", "login-button")
+        
+        username_input.send_keys("standard_user")
+        password_input.send_keys("secret_sauce")
+        login_button.click()
+        
+        assert "Products" in driver.page_source
+    finally:
+        driver.quit()
